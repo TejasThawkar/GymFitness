d0697311d711137b6c2dcaa3839889db8002cc85
Tejas Thawkar <ttthawkar17@gmail.com>
2025-01-14 01:46:32 +0530

    Updated features and enroll

--- astarfitness/authapp/admin.py
+++ astarfitness/authapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from authapp.models import Contact, Enrollment, Trainer, MembershipPlan
 # Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(Enrollment)
+admin.site.register(Trainer)
+admin.site.register(MembershipPlan)
--- astarfitness/authapp/urls.py
+++ astarfitness/authapp/urls.py
@@ -2,4 +2,9 @@ from django.urls import path,include
 from authapp import views
 urlpatterns = [
     path('',views.Home,name='Home'),
+    path('signup',views.signup,name='signup'),
+    path('login',views.handlelogin,name='handlelogin'),
+    path('logout',views.handleLogout,name='handleLogout'),
+    path('contact',views.contact,name='contact'),
+    path('join',views.enroll,name='enroll'),
 ]
